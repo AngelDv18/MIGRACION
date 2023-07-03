6a134b03481ecb77ff30477f5419a915fc113ef5
AngelDv18 <133921931+AngelDv18@users.noreply.github.com>
2023-07-03 17:30:16 -0500

    cambios nuevosxd

--- Empleado23CVAngel/MainWindow.xaml.cs
+++ Empleado23CVAngel/MainWindow.xaml.cs
@@ -48,13 +48,98 @@ namespace Empleado23CVAngel
                 txtCorreo.Clear();
                 MessageBox.Show("REGISTRO EXITOSO");
             }
-
         }
-
         private void BtnRead_Click(object sender, RoutedEventArgs e)
+        {    //int Id = int.Parse(txtId.Text);
+            //Empleado empleado = services.Read(Id);
+            int Id;
+            if (int.TryParse(txtId.Text, out Id))
+            {
+                Empleado empleado = services.Read(Id);
+                if (empleado != null)
+                { // Mostrar la información del empleado en controles de texto
+                    txtNombre.Text = empleado.Nombre;
+                    txtApellido.Text = empleado.Apellido;
+                    txtCorreo.Text = empleado.Correo;
+                    txtFecha.Text = empleado.FechaRegistro.ToString();
+                    // Otros campos si los hay
+
+                    // Puedes mostrar un mensaje de éxito si lo deseas
+                    MessageBox.Show("Empleado encontrado");
+                }
+                else
+                {  // Mostrar un mensaje si el empleado no se encontró
+                    MessageBox.Show("Empleado no encontrado");
+                }
+            }
+            else
+            { // Mostrar un mensaje de error si el valor del Id no es un número válido
+                MessageBox.Show("Ingrese un valor numérico válido para el Id");
+            }
+        }
+        private void BtnEdit_Click(object sender, RoutedEventArgs e)
         {
-            int Id = int.Parse(txtId.Text);
-            Empleado empleado = services.Red(Id);
+            int id;
+            if (int.TryParse(txtId.Text, out id))
+            {
+                Empleado empleadoExistente = services.Read(id);
+                if (empleadoExistente != null)
+                {
+                    empleadoExistente.Nombre = txtNombre.Text;
+                    empleadoExistente.Apellido = txtApellido.Text;
+                    empleadoExistente.Correo = txtCorreo.Text;
+                    services.Update(empleadoExistente);
+                    MessageBox.Show("Empleado actualizado exitosamente");
+                    txtNombre.Clear();
+                    txtApellido.Clear();
+                    txtCorreo.Clear();
+                    txtFecha.Clear();
+                }
+                else
+                {
+                    MessageBox.Show("Empleado no encontrado");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Ingrese un valor numérico válido para el Id");
+            }
+        }
+        private void BtnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            int Id;
+            if (int.TryParse(txtId.Text, out Id))
+            {
+                if (MessageBox.Show("¿Estás seguro de que deseas eliminar este empleado?", "Confirmación", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+                {
+                    bool result = services.Delete(Id);
+                    if (result)
+                    {
+                        // Limpiar los controles de texto
+                        txtNombre.Clear();
+                        txtApellido.Clear();
+                        txtCorreo.Clear();
+                        txtFecha.Clear();
+
+                        MessageBox.Show("Empleado eliminado correctamente");
+                    }
+                    else
+                    {
+                        MessageBox.Show("No se pudo eliminar el empleado");
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Ingrese un valor numérico válido para el Id");
+            }
+        }
+        private void BtnClear_Click(object sender, RoutedEventArgs e)
+        {
+            txtNombre.Clear();
+            txtApellido.Clear();
+            txtCorreo.Clear();
+            txtFecha.Clear();
         }
     }
 }
--- Empleado23CVAngel/Services/EmpleadoServices.cs
+++ Empleado23CVAngel/Services/EmpleadoServices.cs
@@ -1,10 +1,15 @@
 ﻿using Empleado23CVAngel.Context;
 using Empleado23CVAngel.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using MySql.Data.MySqlClient;
+using MySqlX.XDevAPI;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace Empleado23CVAngel.Services
 {
@@ -32,20 +37,59 @@ namespace Empleado23CVAngel.Services
                 throw new Exception("Sucedió un error" + ex.Message);
             }
         }
-        public Empleado Red(int Id)
+        public Empleado Read(int Id)
         {
             try
             {
-                Empleado empleado = new Empleado();
+
                 using (var _context = new ApplicationDbContext())
                 {
-                    empleado = _context.Empleados.Find(Id);
+                    Empleado empleado = _context.Empleados.Find(Id);
                     return empleado;
                 }
             }
             catch (Exception ex)
             {
-                throw new Exception("ERROR: " + ex.Message);
+                throw new Exception("Sucedio un Error: " + ex.Message);
+            }
+        }
+        public void Update(Empleado empleado)
+        {
+            try
+            {
+                using (var _context = new ApplicationDbContext())
+                {
+                    _context.Entry(empleado).State = EntityState.Modified;
+                    _context.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Sucedió un error al actualizar el empleado: " + ex.Message);
+            }
+        }
+        public bool Delete(int Id)
+        {
+            try
+            {
+                using (var _context = new ApplicationDbContext())
+                {
+                    Empleado empleado = _context.Empleados.Find(Id);
+                    if (empleado != null)
+                    {
+                        _context.Empleados.Remove(empleado);
+                        _context.SaveChanges();
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Sucedió un error: " + ex.Message);
             }
         }
     }
